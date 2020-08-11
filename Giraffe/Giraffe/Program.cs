062222d7eec2cc94c6c8d101ade7afdaf0b04122
rachelv <rachel.vaughan@accruent.com>
2020-08-11 17:26:34 -0500

    first commit, basic setup

---
+++ Giraffe/Giraffe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a man named Sean.");
+            Console.WriteLine("He was 29 years old.");
+            Console.WriteLine("He liked the name Sean.");
+            Console.WriteLine("But he didn't like that he was about to turn 30.");
+
+            Console.ReadLine();
+        }
+    }
+}
