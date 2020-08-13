d843e02322dd9b412f5ae0fb401d7a34cd432d4a
rachelv <rachel.vaughan@accruent.com>
2020-08-13 14:49:08 -0500

    Taking user input and converting to numbers

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -10,15 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string characterName = "Sean";
-            int characterAge = 29;
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("There once was a man named " + characterName + ".");
-            Console.WriteLine("He was " + characterAge + " years old.");
-            Console.WriteLine("He liked the name " + characterName + ".");
-
-            characterAge = 30;
-            Console.WriteLine("But he didn't like that he was about to turn " + characterAge + "." );
+            Console.WriteLine(num1 + num2);
 
             Console.ReadLine();
         }
