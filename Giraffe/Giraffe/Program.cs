968deb38b258c0b35bc6b693d0dd6e6c7bef9749
rachelv <rachel.vaughan@accruent.com>
2020-08-20 17:26:23 -0500

    exception handling

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -10,35 +10,13 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string secretWord = "giraffe";
-            string guess = "";
-            int guessCount = 0;
-            int guessLimit = 3;
-            bool outOfGuesses = false;
+            int[,] numberGrid = {
+               { 1, 2 },
+               { 3, 4 },
+               { 5, 6 }
+            };
 
-            do
-            {
-                if (guessCount < guessLimit)
-                {
-                    Console.Write("Enter guess: ");
-                    guess = Console.ReadLine();
-                    guessCount++;
-                }
-                else
-                {
-                    outOfGuesses = true;
-                }
-            }
-            while (guess != secretWord && !outOfGuesses);
-
-            if (outOfGuesses)
-            {
-                Console.Write("Oh no, you ran out of guesses!");
-            }
-            else
-            {
-                Console.Write("You guessed right!");
-            }
+            Console.WriteLine(numberGrid[0, 0]);
 
             Console.ReadLine();
         }
